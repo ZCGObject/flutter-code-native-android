9cd607945d50b1f50622f8c9aecfa675fd39b855
ZCG <1466367025@qq.com>
2020-12-03 15:09:53 +0800

    增加空行

--- app/src/main/java/com/ikang/flutterzcgfourdemo/MainActivity.java
+++ app/src/main/java/com/ikang/flutterzcgfourdemo/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        
 
         textView = findViewById(R.id.params);
 
